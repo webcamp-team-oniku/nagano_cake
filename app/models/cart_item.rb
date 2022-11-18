aa7fd13b0492b3170084ca2b3df290b083c402bc
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-11-18 08:26:12 +0000

    カート機能作成(未完)

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,12 @@ class CartItem < ApplicationRecord
   
   belongs_to :customer
   belongs_to :item
+  validates :amount, presence: true
+  
+  def subtotal
+    item.add_tax_price*amount　#小計金額
+  end
   
 end
+
+
