a2df9ad082691034b8c2242dfa11d295d0ca97a9
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-11-19 00:37:46 +0000

    顧客一覧画面完成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,14 @@
 class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.page(params[:page])
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
