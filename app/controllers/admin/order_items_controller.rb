713fc10753b701da963bfa52d679695bb09e2c2b
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-11-19 09:29:03 +0000

    管理者側topページ/注文履歴詳細ページ作成

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,27 @@
 class Admin::OrderItemsController < ApplicationController
-end
+  
+  def update
+    @order = Order.find(params[:order_id])
+    @order_item = OrderItems.find(params[:id])
+    @order_items = @order.order_item.all
+    
+    is_updated = true
+    if @order_item.update(order_item_params)
+      @order_item.update(status: 2) if @order_item.status == "in_production"# 製作ステータスを製作中にすると、注文ステータスを製作中に自動更新
+      @order_items.each do |order_item|
+        if order_item.status != "completed" # 製作ステータスが全て製作完了ではない場合 
+          is_updated = false # 上記で定義してあるis_updatedをfalseに変更
+        end
+      end
+      @order.update(status: 3) if is_updated #製作ステータスが全て製作完了になれば注文ステータスが発送準備中に自動更新
+    end
+    redirect_to request.referer
+  end
+
+
+private
+  def order_item_params
+      params.require(:order_item).permit(:status)
+  end
+  
+
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,24 @@
 class Admin::OrdersController < ApplicationController
+
+  def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order_items = OrderItem.where(order_id: params[:id])
+    if @order.update(order_params)
+      @order.order_items.update_all(status: 1) if @order.status == "is_waiting" 
+      #入金確認したら自動更新で製作ステータスが全て製作待ちになる
+    redirect_to request.referer
+  end
+
+
+  private
+
+  def order_params
+    params.require(:orders).permit(:status)
+  end
 end
+@order.order_details.update_all(product_status: 1)
