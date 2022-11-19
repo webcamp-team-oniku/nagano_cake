3c0e8196ea062312e9708997e2a4cb3d4a45d1d1
NaohiroFujiwara <112791193+naohiro0217@users.noreply.github.com>
2022-11-19 06:17:43 +0000

    seeds.rbに管理者情報記述

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -60,6 +60,6 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
   def after_sign_up_path_for(resource)
-    customers_path(resource)
+    customers_path
   end
 end
