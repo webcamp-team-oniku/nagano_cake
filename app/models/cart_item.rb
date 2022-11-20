22b25de8c8e7a2d811e385ef441070633b7888a3
EC2 Default User <ec2-user@ip-172-31-36-53.ap-northeast-1.compute.internal>
2022-11-20 08:23:04 +0000

    商品詳細とカートの修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,13 +1,13 @@
 class CartItem < ApplicationRecord
-  
+
   belongs_to :customer
   belongs_to :item
   validates :amount, presence: true
-  
+
   def subtotal
-    item.add_tax_price*amount　#小計金額
+    item.add_tax_price*amount #小計金額
   end
-  
+
 end
 
 
