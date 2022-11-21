6952efda8129641f85765140128bf565d2119692
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-11-21 10:06:11 +0000

    ジャンル検索機能追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,9 +2,12 @@ class Public::HomesController < ApplicationController
   def top
     @items = Item.all
     @items_new = @items.order(created_at: :DESC).limit(4) #created_atで制作日時、DESCで降順
+    @genres = Genre.all
   end
   
   def about
   end
+  
+  
 
 end
