e275a7698fb60251795146541d0d5381b4a90639
EC2 Default User <ec2-user@ip-172-31-36-53.ap-northeast-1.compute.internal>
2022-11-21 10:06:50 +0000

    配送先一覧画面作成

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,5 +1,13 @@
 class Delivery < ApplicationRecord
-  
+
   belongs_to :customer
-  
+
+  def address_display
+  '〒' + postal_code + ' ' + address + ' ' + name
+  end
+
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+
 end
