adc70f9d59b8940bb92eed31ece13d17a8ba87e0
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-11-24 09:59:31 +0000

    ステータス連動修正

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -10,8 +10,7 @@ class Admin::OrderItemsController < ApplicationController
     # 製作ステータスを製作中にすると、注文ステータスを製作中に自動更新
     @order.update(status: "in_production") if order_item.making_status == "is_making"
     #製作ステータスが全て製作完了になれば注文ステータスが発送準備中に自動更新
-    @order.update(status: "preparing_shipment")  if order_item.making_status == "completed"
-
+    @order.update(status: "preparing_shipment")  if @order.order_items.count  == @order.order_items.where(making_status: "completed").count
     redirect_to admin_order_path(@order)
   end
 
