01fd749c609858859fe85705d7482fa85ad49e32
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-11-27 03:00:08 +0000

    カート機能修正２

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,27 +7,17 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    if cart_item = current_customer.cart_items.find_by(item_id:cart_item_params[:item_id])
-      cart_item.amount
-      cart_item_params[:amount].to_i
-      cart_item.save
-      redirect_to cart_items_path
-    else cart_item = CartItem.new(cart_item_params)
-      cart_item.customer_id = current_customer.id
-      cart_item.save
-      redirect_to cart_items_path
-    end
-    
     @cart_item = current_customer.cart_items.build(cart_item_params)
     @cart_items = current_customer.cart_items.all
     @cart_items.each do |cart_item|
       if cart_item.item_id == @cart_item.item_id
-        new_amount = cart_item.amount + @cart_item.amount
+        new_amount = cart_item.amount += @cart_item.amount
         cart_item.update_attribute(:amount, new_amount)
         @cart_item.delete
       end
     end
     @cart_item.save
+    redirect_to cart_items_path
   end
 
   def update
